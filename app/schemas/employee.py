706208088e2d92fd689404756d8da634e7facbef
rajan_kant <kashyaprajan777@gmail.com>
2026-07-12 23:46:52 +0530

    Sprint 1 CRUD Operation Closed.

--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -3,5 +3,11 @@ from pydantic import BaseModel
 class EmployeeCreate(BaseModel):
     first_name: str
     last_name: str
-    Email_id: str
+    email_id: str
     department: str
+
+class EmployeeUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email_id: str | None = None
+    department: str | None = None
